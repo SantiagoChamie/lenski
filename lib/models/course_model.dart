33666c4dd90a850de998b3d06fe71503bd3b410c
SantiagoChamie <s.chamie@uniandes.edu.co>
2025-03-03 09:57:07 -0500

    create course repository

--- lib/models/course_model.dart
+++ lib/models/course_model.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 class Course {
+  int? id;
   String name;
   String level;
   String code;
@@ -8,11 +9,11 @@ class Course {
   bool speaking;
   bool reading;
   bool writing;
-  int position; 
   Color color;
   String imageUrl;
 
   Course({
+    this.id,
     required this.name,
     required this.level,
     required this.code,
@@ -20,8 +21,38 @@ class Course {
     required this.speaking,
     required this.reading,
     required this.writing,
-    required this.position,
     required this.color,
     required this.imageUrl,
   });
+  // Convert a Course into a Map. The keys must correspond to the names of the columns in the database.
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      'name': name,
+      'level': level,
+      'code': code,
+      'listening': listening ? 1 : 0,
+      'speaking': speaking ? 1 : 0,
+      'reading': reading ? 1 : 0,
+      'writing': writing ? 1 : 0,
+      'color': color.value,
+      'imageUrl': imageUrl,
+    };
+  }
+
+  // Extract a Course object from a Map.
+  factory Course.fromMap(Map<String, dynamic> map) {
+    return Course(
+      id: map['id'],
+      name: map['name'],
+      level: map['level'],
+      code: map['code'],
+      listening: map['listening'] == 1,
+      speaking: map['speaking'] == 1,
+      reading: map['reading'] == 1,
+      writing: map['writing'] == 1,
+      color: Color(map['color']),
+      imageUrl: map['imageUrl'],
+    );
+  }
 }
