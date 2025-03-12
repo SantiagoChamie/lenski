cc8b9be549e2814d426763e75d530d14225c0450
SantiagoChamie <s.chamie@uniandes.edu.co>
2025-03-12 10:53:20 -0500

    widget documentation

--- lib/utils/proportions.dart
+++ lib/utils/proportions.dart
@@ -6,33 +6,57 @@ class Proportions {
 
   Proportions(this.context);
 
+  /// Returns the width of the screen.
   double screenWidth() => MediaQuery.of(context).size.width;
+
+  /// Returns the height of the screen.
   double screenHeight() => MediaQuery.of(context).size.height;
 
+  /// Returns the width of the main screen excluding the sidebar.
   double mainScreenWidth() => screenWidth() - sidebarWidth();
 
-  double sidebarWidth() => 100; //screenWidth() * 0.0744;
-  double sidebarButtonWidth() => 59;//screenWidth() * 0.0438;
+  /// Returns the width of the sidebar.
+  double sidebarWidth() => 100; // Fixed width for the sidebar
 
-  double standardPadding() => 20; //screenHeight() * 0.025;
+  /// Returns the width of the sidebar button.
+  double sidebarButtonWidth() => 59; // Fixed width for the sidebar button
 
+  /// Returns the standard padding value.
+  double standardPadding() => 20; // Fixed padding value
+
+  /// Returns the height of the create course screen.
   double createCourseHeight() => screenHeight() - 2 * standardPadding();
+
+  /// Returns the width of the create course screen.
   double createCourseWidth() => screenWidth() - sidebarWidth() - 2 * standardPadding();
-  double createCourseBottomHeight() => createCourseHeight()/6;
+
+  /// Returns the height of the bottom section of the create course screen.
+  double createCourseBottomHeight() => createCourseHeight() / 6;
+
+  /// Returns the height of the top section of the create course screen.
   double createCourseTopHeight() => createCourseHeight() - createCourseBottomHeight();
 
-  double createCourseColumnWidth() => createCourseWidth()/3;
-  double createCourseButtonHeight() => createCourseHeight()/12;
-  double createCourseButtonWidth() => (createCourseColumnWidth()-4*standardPadding());
+  /// Returns the width of a column in the create course screen.
+  double createCourseColumnWidth() => createCourseWidth() / 3;
 
+  /// Returns the height of a button in the create course screen.
+  double createCourseButtonHeight() => createCourseHeight() / 12;
+
+  /// Returns the width of a button in the create course screen.
+  double createCourseButtonWidth() => createCourseColumnWidth() - 4 * standardPadding();
+
+  /// Returns the height of the library screen.
   double libraryHeight() => screenHeight() - 2 * standardPadding() - 100;
-  double libraryWidth() => (mainScreenWidth() - 2 * standardPadding() - sidebarWidth())/2;
-  double bookWidth() => libraryWidth()/3 - 4 * standardPadding();
 
+  /// Returns the width of the library screen.
+  double libraryWidth() => (mainScreenWidth() - 2 * standardPadding() - sidebarWidth()) / 2;
 
-  // CourseButton height has parameter Quantity
-  // with quantity = 3, being the maximum allowed
-  double courseButtonHeight(quantity) {
+  /// Returns the width of a book in the library screen.
+  double bookWidth() => libraryWidth() / 3 - 4 * standardPadding();
+
+  /// Returns the height of a course button based on the quantity.
+  /// The maximum allowed quantity is 3.
+  double courseButtonHeight(int quantity) {
     if (quantity <= 3) {
       return (screenHeight() - (3 + quantity - 1) * standardPadding() - sidebarButtonWidth()) / quantity;
     } else {
